e60afcd713412ac22c63aabbe35a133c77182483
Prateek_Rai <prateekr202@gmail.com>
2026-03-21 22:05:47 +0530

    add deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  server: {
-    allowedHosts: [
-      'exam-bureau-terminal-chevy.trycloudflare.com',
-      'current-laboratory-trusts-poetry.trycloudflare.com'
-    ]
-  }
+  base: '/My-Portfolio-/',
 })
